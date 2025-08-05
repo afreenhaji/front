7c8676b9e50608b15359c66c9a8183d765607a2a
afreenhaji <161109294+afreenhaji@users.noreply.github.com>
2025-08-05 23:14:15 +0530

    Updated frontend

--- backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { isAuth } from "../middlewares/isAuth.js";
-import { updateBio } from "../controllers/userControllers.js";
-
-const router = express.Router();
-
-router.put("/update-bio", isAuth, updateBio);
-
-export default router;
